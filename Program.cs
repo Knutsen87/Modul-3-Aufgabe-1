9604f211944c453e0a526e6237cf67c2e45fcb1b
Dennis Gillich <dennisgillich@live.de>
2022-02-10 17:09:34 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Bitte ebene Sie ihre erste Zahl ein: ");
+double Zahl1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Bitte geben Sie ihre zweite Zahl ein: \n");
+double Zahl2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("{0} + {1} = {2}", Zahl1, Zahl2, Zahl1 + Zahl2);
+Console.WriteLine("{0} - {1} = {2}", Zahl1, Zahl2, Zahl1 - Zahl2);
+Console.WriteLine("{0} * {1} = {2}", Zahl1, Zahl2, Zahl1 * Zahl2);
+Console.WriteLine("{0} / {1} = {2}", Zahl1, Zahl2, Zahl1 / Zahl2);
+Console.WriteLine("{0} % {1} = {2}", Zahl1, Zahl2, Zahl1 % Zahl2);
+
+Console.ReadKey();  
+
+
